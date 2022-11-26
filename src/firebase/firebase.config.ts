4d53b31414c887d5ec05da29609e5898e5990f61
Amedeo Corsi <corsiamedeo@gmail.com>
2022-11-26 16:57:42 +0000

    moved all firebase config stuff to .env file

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIRE_BASE_API_KEY,
-  authDomain: 'properties-management-dff37.firebaseapp.com',
-  projectId: 'properties-management-dff37',
-  storageBucket: 'properties-management-dff37.appspot.com',
-  messagingSenderId: '433742168897',
-  appId: '1:433742168897:web:921502b50da0ef3529d7c0',
-  measurementId: 'G-V2JK12C1HM',
+  authDomain: process.env.REACT_APP_FIRE_BASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIRE_BASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRE_BASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRE_BASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRE_BASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIRE_BASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
